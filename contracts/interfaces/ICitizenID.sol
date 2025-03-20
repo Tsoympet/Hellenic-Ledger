e144ffcfc1f1bb41d881e4a2f30a47286efc0159
Tsoympet <tsoympet7@gmail.com>
2025-03-20 17:09:07 +0200

    Update ICitizenID.sol
    Interface for the identity verification contract.

--- contracts/interfaces/ICitizenID.sol
+++ contracts/interfaces/ICitizenID.sol
@@ -1,1 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
 
+interface ICitizenID {
+    function isVerified(address user) external view returns (bool);
+    function verifyUser(address user) external;
+    function revokeVerification(address user) external;
+}
