35f87c00bec27d22050286d4e0e667561130f2ee
Tsoympet <tsoympet7@gmail.com>
2025-03-20 17:12:18 +0200

    Update ILiquidityPoolManager.sol
    Interface for the liquidity pool manager contract.

--- contracts/interfaces/ILiquidityPoolManager.sol
+++ contracts/interfaces/ILiquidityPoolManager.sol
@@ -1,1 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
 
+interface ILiquidityPoolManager {
+    function addLiquidity(address tokenA, address tokenB, uint256 amountA, uint256 amountB) external;
+}
