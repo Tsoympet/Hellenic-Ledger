6293191f06a25481a24c5f012e0a807b314962e3
Tsoympet <tsoympet7@gmail.com>
2025-03-20 17:03:46 +0200

    Update QuestNFT.sol
    NFT rewarding Obolos/Drachma engagement.

--- contracts/supporting/QuestNFT.sol
+++ contracts/supporting/QuestNFT.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
 
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+
+contract QuestNFT is Initializable, OwnableUpgradeable, ERC721Upgradeable {
+    address public obolos;
+    address public drachma;
+    uint256 public tokenIdCounter;
+
+    event Minted(address indexed to, uint256 tokenId);
+
+    function initialize(address _obolos, address _drachma) external initializer {
+        __ERC721_init("QuestNFT", "QST");
+        __Ownable_init(msg.sender); // Owned by Politeia post-deployment
+        obolos = _obolos;
+        drachma = _drachma;
+        tokenIdCounter = 1;
+    }
+
+    function mint(address to) external onlyOwner {
+        _safeMint(to, tokenIdCounter);
+        emit Minted(to, tokenIdCounter);
+        tokenIdCounter++;
+    }
+}
