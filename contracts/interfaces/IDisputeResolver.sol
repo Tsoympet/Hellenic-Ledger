954a1cc811c9df262c6078b8b17bffe82ca4f8e9
Tsoympet <tsoympet7@gmail.com>
2025-03-20 17:12:50 +0200

    Update IDisputeResolver.sol
    Interface for the dispute resolution contract.

--- contracts/interfaces/IDisputeResolver.sol
+++ contracts/interfaces/IDisputeResolver.sol
@@ -1,1 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
 
+interface IDisputeResolver {
+    function fileDispute(address respondent, uint256 amount, string calldata reason) external;
+    function resolveDispute(uint256 disputeId, bool approve) external;
+}
