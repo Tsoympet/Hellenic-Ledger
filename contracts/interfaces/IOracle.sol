934e81a19ad3592f8c1e1b327dd5a33d4a9cea3a
Tsoympet <tsoympet7@gmail.com>
2025-03-20 17:11:57 +0200

    Update IOracle.sol
    Interface for the price oracle contract.

--- contracts/interfaces/IOracle.sol
+++ contracts/interfaces/IOracle.sol
@@ -1,1 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
 
+interface IOracle {
+    function updatePrice(address token, uint256 price) external;
+    function getPrice(address token) external view returns (uint256);
+}
