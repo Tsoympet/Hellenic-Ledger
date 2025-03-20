e59a93e9287308c779a760d8126e7fafeea2981d
Tsoympet <tsoympet7@gmail.com>
2025-03-20 17:10:03 +0200

    Update IPerksManager.sol
    Interface for the voting boost manager contract.

--- contracts/interfaces/IPerksManager.sol
+++ contracts/interfaces/IPerksManager.sol
@@ -1,1 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
 
+interface IPerksManager {
+    function updateBoost(address user, uint256 amount) external;
+    function getVotingBoost(address user) external view returns (uint256);
+}
