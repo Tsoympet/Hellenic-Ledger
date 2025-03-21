39070133922ba5d81f463ed4dc743919e61c6606
Tsoympet <tsoympet7@gmail.com>
2025-03-21 21:20:22 +0200

    Create Payments.js
    Payments screen.

---
+++ app/src/screens/Payments.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View } from 'react-native';
+import PaymentForm from '../components/PaymentForm';
+import { payTalanton, payDrachma, payObolos } from '../utils/web3';
+
+const Payments = ({ signer }) => {
+  const handlePay = async (token, recipient, amount) => {
+    if (token === "Talanton") await payTalanton(signer, recipient, amount);
+    else if (token === "Drachma") await payDrachma(signer, recipient, amount);
+    else if (token === "Obolos") await payObolos(signer, recipient, amount);
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <PaymentForm tokenName="Talanton" onPay={(recipient, amount) => handlePay("Talanton", recipient, amount)} />
+      <PaymentForm tokenName="Drachma" onPay={(recipient, amount) => handlePay("Drachma", recipient, amount)} />
+      <PaymentForm tokenName="Obolos" onPay={(recipient, amount) => handlePay("Obolos", recipient, amount)} />
+    </View>
+  );
+};
+
+export default Payments;
